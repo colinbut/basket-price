0e3903a167dd2280cbab9c20601d65f0949488b6
Colin But <colin.but@hotmail.co.uk>
2015-04-02 20:58:50 +0100

    Added skeleton project files

---
+++ src/main/java/com/mycompany/basket_price/BasketItem.java
@@ -0,0 +1,12 @@
+/**
+ * 
+ */
+package com.mycompany.basket_price;
+
+/**
+ * @author colin
+ *
+ */
+public interface BasketItem {
+
+}
---
+++ src/main/java/com/mycompany/basket_price/PriceBasketApplicationOutputWriter.java
@@ -0,0 +1,12 @@
+/**
+ * 
+ */
+package com.mycompany.basket_price;
+
+/**
+ * @author colin
+ *
+ */
+public class PriceBasketApplicationOutputWriter {
+
+}
