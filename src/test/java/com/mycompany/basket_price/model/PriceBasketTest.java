edc08144376957f1e521597089c1336f9fc1271f
Colin But <colinbut@users.noreply.github.com>
2015-05-12 22:28:53 +0100

    more unit tests

--- src/test/java/com/mycompany/basket_price/model/PriceBasketTest.java
+++ src/test/java/com/mycompany/basket_price/model/PriceBasketTest.java
@@ -9,29 +9,40 @@ import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
 
+import com.mycompany.basket_price.model.goods.Apples;
+import com.mycompany.basket_price.model.goods.Bread;
+
 /**
  * @author colin
  *
  */
 public class PriceBasketTest {
 
-	/**
-	 * @throws java.lang.Exception
-	 */
+	PriceBasket priceBasket;
+
 	@Before
-	public void setUp() throws Exception {
+	public void setUp(){
+		priceBasket = new PriceBasket();
 	}
-
-	/**
-	 * @throws java.lang.Exception
-	 */
-	@After
-	public void tearDown() throws Exception {
-	}
-
+	
 	@Test
-	public void test() {
-		fail("Not yet implemented");
+	public void testPutItemInBasket() {
+		
+		Apples apples = new Apples();
+		priceBasket.putItemInBasket(apples);
+		assertTrue(priceBasket.getBasketItems().containsKey(apples));
+		
+		priceBasket.putItemInBasket(new Apples());
+		assertEquals(1, priceBasket.getBasketItems().size());
+		
+	}
+	
+	@Test
+	public void testBasketContainItem(){
+		
+		priceBasket.getBasketItems().put(new Bread(), 3);
+		assertTrue(priceBasket.basketContainItem(new Bread()));
+		
 	}
 
 }
--- src/test/java/com/mycompany/basket_price/model/ReceiptTest.java
+++ src/test/java/com/mycompany/basket_price/model/ReceiptTest.java
@@ -3,10 +3,84 @@
  */
 package com.mycompany.basket_price.model;
 
+import static org.junit.Assert.*;
+
+import java.math.BigDecimal;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+import org.junit.Before;
+import org.junit.Test;
+
+import com.mycompany.basket_price.model.Receipt.ReceiptBuilder;
+import com.mycompany.basket_price.model.goods.Apples;
+import com.mycompany.basket_price.model.goods.Bread;
+import com.mycompany.basket_price.model.goods.Milk;
+import com.mycompany.basket_price.model.goods.Soup;
+
 /**
  * @author colin
  *
  */
 public class ReceiptTest {
 
+	private ReceiptBuilder receiptBuilder;
+	
+	@Before
+	public void setUp(){
+		receiptBuilder = new ReceiptBuilder();
+	}
+	
+	
+	@Test(expected=IllegalArgumentException.class)
+	public void testReceiptBuilderBuildNotProperly(){
+		receiptBuilder.build();		
+	}
+	
+	@Test
+	public void testReceiptBuilderBuildSuccess(){
+		
+		@SuppressWarnings("serial")
+		Receipt receipt = receiptBuilder.withSubTotal(new BigDecimal(2.00))
+						.withTotal(new BigDecimal(3.00))
+						.withItems(new ArrayList<PricedBasketItem>()
+								{
+									{
+										add(new Apples());
+										add(new Bread());
+										add(new Milk());
+										add(new Soup());
+									}
+								}
+								)
+						.withSpecialOffersApplied(new HashMap<SpecialOffer, BigDecimal>()
+								{
+									{
+										put(new SpecialOfferDiscount(20.0, new Apples()), new BigDecimal(1.0));
+									}
+								}
+							).build();
+		
+		
+		assertTrue(receipt instanceof Receipt);
+		
+		List<PricedBasketItem> items = receipt.getItems();
+		Map<SpecialOffer, BigDecimal> specialOffersApplied = receipt.getSpecialOffersApplied();
+		
+		BigDecimal subTotal = receipt.getSubtotal();
+		BigDecimal total = receipt.getTotal();
+		
+		assertEquals(new BigDecimal(2.00), subTotal);
+		assertEquals(new BigDecimal(3.00), total);
+		
+		for(PricedBasketItem item : items){
+			// TODO
+		}
+		
+		
+	}
+	
+	
 }
