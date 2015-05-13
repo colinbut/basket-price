44858cdfa1f0f293d152317f933044068fa41eb1
Colin But <colinbut@users.noreply.github.com>
2015-05-13 20:30:11 +0100

    more junits

--- src/test/java/com/mycompany/basket_price/model/SpecialOfferBuy2Get1HalfPriceTest.java
+++ src/test/java/com/mycompany/basket_price/model/SpecialOfferBuy2Get1HalfPriceTest.java
@@ -3,10 +3,52 @@
  */
 package com.mycompany.basket_price.model;
 
+import static org.junit.Assert.*;
+
+import java.math.BigDecimal;
+import java.util.HashMap;
+import java.util.Map;
+
+import org.junit.Before;
+import org.junit.Test;
+
+import com.mycompany.basket_price.model.goods.Apples;
+import com.mycompany.basket_price.model.goods.Bread;
+import com.mycompany.basket_price.model.goods.Milk;
+import com.mycompany.basket_price.model.goods.Soup;
+
 /**
  * @author colin
  *
  */
 public class SpecialOfferBuy2Get1HalfPriceTest {
 
+	private SpecialOfferBuy2Get1HalfPrice specialOffer;
+	
+	@Before
+	public void setUp(){
+		specialOffer = new SpecialOfferBuy2Get1HalfPrice(new Bread(), new Soup());
+	}
+	
+	@Test
+	public void testHandleSpecialOffer(){
+		
+		Bread bread = new Bread();
+		Soup soup = new Soup();
+		
+		PriceBasket basket = new PriceBasket();
+		basket.putItemInBasket(new Bread());
+		basket.putItemInBasket(new Apples());
+		basket.putItemInBasket(bread);
+		basket.putItemInBasket(soup);
+		basket.putItemInBasket(new Milk());
+		
+		Map<SpecialOffer, BigDecimal> specialOffersApplied = new HashMap<SpecialOffer, BigDecimal>();
+		
+		specialOffersApplied = specialOffer.handleSpecialOffer(specialOffersApplied, basket, bread);
+		
+		assertEquals(new BigDecimal(0.80), specialOffersApplied.get(specialOffer));
+		
+		
+	}
 }
--- src/test/java/com/mycompany/basket_price/model/SpecialOfferDiscountTest.java
+++ src/test/java/com/mycompany/basket_price/model/SpecialOfferDiscountTest.java
@@ -3,10 +3,38 @@
  */
 package com.mycompany.basket_price.model;
 
+import static org.junit.Assert.*;
+
+import java.math.BigDecimal;
+import java.util.HashMap;
+import java.util.Map;
+
+import org.junit.Before;
+import org.junit.Test;
+
+import com.mycompany.basket_price.model.goods.Apples;
+
 /**
  * @author colin
  *
  */
 public class SpecialOfferDiscountTest {
 
+	private SpecialOfferDiscount specialOfferDiscount;
+	
+	@Before
+	public void setUp(){
+		specialOfferDiscount = new SpecialOfferDiscount(50, new Apples());
+	}
+	
+	@Test
+	public void testHandleSpecialOfferDiscount(){
+		
+		Map<SpecialOffer, BigDecimal> specialOffersApplied = new HashMap<SpecialOffer, BigDecimal>(); 
+				
+		specialOffersApplied = specialOfferDiscount.handleSpecialOffer(specialOffersApplied);
+		
+		assertEquals(new BigDecimal(0.5),specialOffersApplied.get(specialOfferDiscount));
+		
+	}
 }
